1dae6de75bb975a4a3ffcdc38fdb342ac944ee7b
dkeeslingx2594 <36906113+dkeeslingx2594@users.noreply.github.com>
2018-04-05 20:02:08 -0500

    Update pythonex1.py

--- pythonex1.py
+++ pythonex1.py
@@ -1,12 +1,1 @@
-name = raw_input("What is your name? ")
-print "Hello, " + name + "!"
 
-name = raw_input("WHAT IS YOUR NAME? ")
-new_string = "HELLO, "+ name + "!"
-print new_string.upper()
-print "YOUR NAME HAS " + str(len(name)) + " LETTERS IN IT!"
-
-name= raw_input("Give me a name. ")
-job= raw_input("Give me a job. ")
-job2= raw_input("Give me another job. ")
-print "%s is a %s trying to become an %s" % (name, job, job2)
