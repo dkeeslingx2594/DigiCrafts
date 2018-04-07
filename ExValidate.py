3e6b10b427e4861ed5720d6375e9d6bdd53e50f9
dkeeslingx2594 <36906113+dkeeslingx2594@users.noreply.github.com>
2018-04-07 10:15:36 -0500

    Update ExValidate.py

--- ExValidate.py
+++ ExValidate.py
@@ -1,6 +1,1 @@
-strings = ["Hello", "I", "am", "a", "IT", "builder", "am", "I", "cool", "?"]
-newstrings = []
-for string in strings:
-    if string not in newstrings:
-        newstrings.append(string)
-print(newstrings)
+
